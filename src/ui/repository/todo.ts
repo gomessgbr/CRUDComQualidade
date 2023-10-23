361ee0cd1c4f315a8cbbc7bc8b0747dc174c32a5
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-10-23 10:02:10 -0300

    feat: typed return backend

--- src/ui/repository/todo.ts
+++ src/ui/repository/todo.ts
@@ -14,7 +14,9 @@ function get({
 }: TodoRepositoryGetParams): Promise<TodoRepositoryGetOutput> {
   return fetch("/api/todos").then(async (respostaDoServidor) => {
     const todosString = await respostaDoServidor.text();
-    const todosFromServer = JSON.parse(todosString).todos;
+
+    const todosFromServer = parseTodosFromServer(JSON.parse(todosString)).todos;
+
     const ALL_TODOS = todosFromServer;
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
@@ -38,4 +40,37 @@ interface Todo {
   content: string;
   date: Date;
   done: boolean;
+}
+
+function parseTodosFromServer(responseBody: unknown): { todos: Array<Todo> } {
+  if (
+    responseBody !== null &&
+    typeof responseBody === "object" &&
+    "todos" in responseBody &&
+    Array.isArray(responseBody.todos)
+  ) {
+    return {
+      todos: responseBody.todos.map((todo: unknown) => {
+        if (todo == null && typeof todo !== "object") {
+          throw new Error("Invalid todo from API");
+        }
+
+        const { id, content, done, date } = todo as {
+          id: string;
+          content: string;
+          done: boolean;
+          date: string;
+        };
+        return {
+          id,
+          content,
+          done: String(done).toLowerCase() === "true",
+          date: new Date(date),
+        };
+      }),
+    };
+  }
+  return {
+    todos: [],
+  };
 }
