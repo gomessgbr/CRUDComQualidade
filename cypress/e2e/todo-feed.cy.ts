a8fdb8707dcb413ae247d3963e399a46b87fc055
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-11-18 12:05:43 -0300

    feat: test

--- cypress/e2e/todo-feed.cy.ts
+++ cypress/e2e/todo-feed.cy.ts
@@ -1,5 +1,29 @@
-describe("todos feed", () => {
+const BASE_URL = "http://localhost:3000";
+
+describe("/ - Todos feed", () => {
   it("when load, renders the page", () => {
-    cy.visit("http://localhost:3000/");
+    cy.visit(BASE_URL);
+  });
+  it.only("when create a anew todo, it must appears in the screen", () => {
+    cy.intercept("POST", `${BASE_URL}/api/todos`, (request) => {
+      request.reply({
+        statusCode: 201,
+        body: {
+          todd: {
+            id: "050a43bc-6424-4096-8923-b9995d22a50d",
+            content: "Test todo",
+            date: "2023-11-18T14:53:45.702Z",
+            done: false,
+          },
+        },
+      });
+    }).as("createTodo");
+    cy.visit(BASE_URL);
+    const $inputAddTodo = cy.get("input[name='add-todo']");
+    $inputAddTodo.type("Test todo");
+    const $btnAddTodo = cy.get("[aria-label='Adicionar novo item']");
+    $btnAddTodo.click();
+
+    cy.get("table > tbody").contains("Test todo");
   });
 });
