4d8c1f06a7ab1335b387e0c4bd21d94f3644776c
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-11-23 21:56:58 -0300

    lint: fix lint erros

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     video: false,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
