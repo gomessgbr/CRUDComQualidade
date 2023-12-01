05b7b6d5012b894ad75c1a947aabafd3fbddf474
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-12-01 10:32:33 -0300

    test: description

--- cypress/e2e/todo-feed.cy.ts
+++ cypress/e2e/todo-feed.cy.ts
@@ -4,7 +4,7 @@ describe("/ - Todos feed", () => {
   it("when load, renders the page", () => {
     cy.visit(BASE_URL);
   });
-  it("when create a anew todo, it must appears in the screen", () => {
+  it("when create a new todo, it must appears in the screen", () => {
     cy.intercept("POST", `${BASE_URL}/api/todos`, (request) => {
       request.reply({
         statusCode: 201,
